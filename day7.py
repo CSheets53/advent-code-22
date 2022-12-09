d49a7659bb08363282e4c6a9e8f2537f11e287dc
Cooper Sheets <coopersheets@gmail.com>
2022-12-09 13:42:18 -0600

    Finished day 7

--- day7.py
+++ day7.py
@@ -1,37 +1,57 @@
+from collections import defaultdict
+import re
+import sys
+
 terminal_output = []
 with open("./inputs/d7.txt") as f:
     for line in f:
         terminal_output.append(line[:-1]) # cut \n
 
-terminal_output = ["$ cd /",
-"$ ls",
-"dir a",
-"14848514 b.txt",
-"8504156 c.dat",
-"dir d",
-"$ cd a",
-"$ ls",
-"dir e",
-"29116 f",
-"2557 g",
-"62596 h.lst",
-"$ cd e",
-"$ ls",
-"584 i",
-"$ cd ..",
-"$ cd ..",
-"$ cd d",
-"$ ls",
-"4060174 j",
-"8033020 d.log",
-"5626152 d.ext",
-"7214296 k"]
+# build filesystem
+dir_sizes = defaultdict(int)
+current_path = []
+
+for line in terminal_output:
+    if line.startswith("$ cd"):
+        # grab dir name
+        dir = line.split()[-1]
+        if dir == "..":
+            # move back up
+            current_path.pop()
+        else:
+            # move dirs
+            if dir == '/':
+                current_path.append("root")
+            else:
+                # move down
+                current_path.append(dir)
+    # check if line starts with a num
+    elif re.match(r"^\d+", line):
+        current_file_size = int(line.split()[0])
+        
+        # update the size for each contained dir
+        for i in range(len(current_path)):
+            dir_sizes['/'.join(current_path[:i+1])] += int(current_file_size)
 
 def part1():
-    return -1
+    # find all dirs with a total size <= 100,000
+    total_sum = 0
+    for dir, size in dir_sizes.items():
+        if size <= 100_000:
+            total_sum += size
+
+    return total_sum
 
 def part2():
-    return -1
+    unused_space = 70_000_000 - dir_sizes["root"]
+    space_to_delete = 30_000_000 - unused_space
 
-print(f"Part 1: {part1()}")
+    smallest_dir_size = sys.maxsize
+    for dir, size in dir_sizes.items():
+        if size >= space_to_delete and size < smallest_dir_size:
+            smallest_dir_size = size
+
+    return smallest_dir_size
+
+#print(f"Part 1: {part1()}")
 print(f"Part 2: {part2()}")
